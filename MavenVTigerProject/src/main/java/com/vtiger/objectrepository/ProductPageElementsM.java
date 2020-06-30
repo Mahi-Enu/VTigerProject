1e08fae49fb8eaae71fe6721429e706bf715c76c
Mahi <Mahi@MahiEnu-PC>
2020-06-30 22:37:52 +0530

    2nd commit

--- MavenVTigerProject/src/main/java/com/vtiger/objectrepository/ProductPageElementsM.java
+++ MavenVTigerProject/src/main/java/com/vtiger/objectrepository/ProductPageElementsM.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.support.FindBy;
 
 public class ProductPageElementsM {
 	
-	@FindBy(css = "img[title='Create Product...']")
+	@FindBy(css = "img[alt='Create Product...']")
 	private WebElement createProducts;
 	
 	public WebElement getCreateProducts()
--- MavenVTigerProject/src/test/java/com/vtiger/testscripts/ProductsCreationMTest.java
+++ MavenVTigerProject/src/test/java/com/vtiger/testscripts/ProductsCreationMTest.java
@@ -8,6 +8,7 @@ import org.testng.annotations.Test;
 
 import com.vtiger.genericlib.PrimaryClassM;
 import com.vtiger.objectrepository.CreateProductsPageElementsM;
+import com.vtiger.objectrepository.HomePageElementsM;
 import com.vtiger.objectrepository.ProductPageElementsM;
 import com.vtiger.objectrepository.ProductsInfoPageElementsM;
 
@@ -16,17 +17,18 @@ public class ProductsCreationMTest extends PrimaryClassM {
 	public void createdProducts() throws IOException
 	
 	{
+		HomePageElementsM hp = PageFactory.initElements(driver, HomePageElementsM.class);
 		ProductPageElementsM pp = PageFactory.initElements(driver, ProductPageElementsM.class);
 		CreateProductsPageElementsM cpp = PageFactory.initElements(driver, CreateProductsPageElementsM.class);
 		ProductsInfoPageElementsM pip = PageFactory.initElements(driver, ProductsInfoPageElementsM.class);
 		
-		
+		hp.getProduct().click();
 		pp.getCreateProducts().click();
-		cpp.getProductname().sendKeys(data.getDataFromExcel("ProductsData", 2, 1));
+		cpp.getProductname().sendKeys(data.getDataFromExcel("ProductData", 2, 1));
 		cpp.getSaveBtn().click();
-		String actProductsMsg = pip.getProductsSuccessMsg().getText();
+		String actProductMsg = pip.getProductsSuccessMsg().getText();
 		
-		Assert.assertTrue(actProductsMsg.contains(data.getDataFromExcel("ProductsData",2, 2)));
+		Assert.assertTrue(actProductMsg.contains(data.getDataFromExcel("ProductData",2, 2)));
 	}
 
 }
