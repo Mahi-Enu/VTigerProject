f752e45574f0b0f79f7ad5c82c5b69b6d87ec295
Mahi <Mahi@MahiEnu-PC>
2020-06-30 23:03:21 +0530

    commit push 2

--- MavenVTigerProject/src/main/java/com/vtiger/objectrepository/ProductPageElementsM.java
+++ MavenVTigerProject/src/main/java/com/vtiger/objectrepository/ProductPageElementsM.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.support.FindBy;
 
 public class ProductPageElementsM {
 	
-	@FindBy(css = "img[alt='Create Product...']")
+	@FindBy(css = "img[title='Create Product...']")
 	private WebElement createProducts;
 	
 	public WebElement getCreateProducts()
